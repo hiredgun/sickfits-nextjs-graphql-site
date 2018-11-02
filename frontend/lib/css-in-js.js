50b248f66f94915deaa07ba8bd9fdf0f728ba0a6
hiredgun <turczyns@gmail.com>
2018-11-02 21:40:25 +0100

    add styled-media-query

--- frontend/lib/css-in-js.js
+++ frontend/lib/css-in-js.js
@@ -1,5 +1,13 @@
 import styled, { css, ThemeProvider } from 'styled-components';
 import { prop, ifProp, switchProp } from 'styled-tools';
+import { generateMedia } from 'styled-media-query';
+
+const media = generateMedia({
+    huge: '1440px',
+    large: '1170px',
+    medium: '768px',
+    small: '450px',
+});
 
 export default styled;
-export { css, prop, ifProp, switchProp, ThemeProvider };
+export { css, prop, ifProp, switchProp, ThemeProvider, media };
